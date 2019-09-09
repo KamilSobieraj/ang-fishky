ef0f8c1d7007220a56c66f93b27cc2129333d9a5
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-09-09 15:33:22 +0200

    Go Back button do QuoteDetailComp added

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -12,7 +12,6 @@ export class AuthorsComponent implements OnInit {
 
   ngOnInit() {
     this.allQuotes = this.authorsService.getAuthorsWithBooks();
-    console.log(this.allQuotes);
   }
 
 }
--- src/app/quotes/quote-card-details/quote-card-details.component.ts
+++ src/app/quotes/quote-card-details/quote-card-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Quotation} from '../../shared/quotation.model';
 import {QuotesService} from '../quotes.service';
 import {ActivatedRoute, Params} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-quote-card-details',
@@ -12,7 +13,8 @@ export class QuoteCardDetailsComponent implements OnInit {
 quotation: Quotation;
 id: number;
   constructor(private quotesService: QuotesService,
-              private router: ActivatedRoute) { }
+              private router: ActivatedRoute,
+              private location: Location) { }
 
   ngOnInit() {
       this.router.params.subscribe((params: Params) => {
@@ -21,4 +23,7 @@ id: number;
     });
   }
 
+  onGoBack() {
+    this.location.back();
+  }
 }
